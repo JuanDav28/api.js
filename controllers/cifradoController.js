ea04911f90303f1d03bf06b55c5f6c75867e06d5
JuanDav28 <juann28sanchez@gmail.com>
2023-06-16 18:21:15 -0500

    middl + jwt

--- controllers/cifradoController.js
+++ controllers/cifradoController.js
@@ -1,47 +1,56 @@
 const bcrypt = require("bcryptjs");
 
-// Método para modificar un usuario
-const updateUser = (userId, newPassword) => {
-      // Obtiene el usuario de la base de datos (id)
-  const user = getUserById(userId);
 
-      //verificacion de que existe user
-  if (!user) {
-    console.log('Usuario no encontrado');
-    return;
-  }
-
-
-        const saltRounds = 10;    // Cifra la nueva contraseña
-
-  bcrypt.hash(newPassword, saltRounds, function(err, hash) {
+  const saltRounds = 10;    // Cifra la nueva contraseña
+  const plainPassword = "juanddas";
+  bcrypt.hash(newPassword, saltRounds, function (err, hash) {
     if (err) {
       console.error(err);
-      return;
     }
-
-    // Actualiza la contraseña del usuario con hash cifrado
-    user.password = hash;
-    saveUser(user);
-
-    console.log('Contraseña modificada correctamente');
+    else{
+      console.log('hash de la contraseña creado', hash)
+    } 
   });
-};
+
+  const hashedPassword = '-';
+  const loginPassword = 'juanddas';
+
+  bcrypt.compare(loginPassword,hashedPassword,function(err, result){
+    if(err){
+        console.error(err);
+    }
+    else if(result){
+        console.log("Contraseña válida");
+    }
+    else{
+        console.log("Contraseña inválida");
+    }
+})
 
 
 
-             // se modifica y cambio usuario (pendiente)
-// Función para obtener un usuario por su ID
-const getUserById = (userId) => {
-  // Se Obtiene el usuario de la base de datos según su ID
-  // y devuelve el objeto de usuario
-};
 
-const saveUser = (user) => {
-};
 
-    // Llama a la función para modificar un usuario
-const userId = '005'; // Reemplaza con el ID del usuario que deseas modificar
-const newPassword = 'juanddas'; // Lo remplaza con la nueva contraseña
 
-updateUser(userId, newPassword);
+
+
+
+
+
+  // no use-----------------------------------------------------------------------------
+
+// // se modifica y cambio usuario (pendiente)
+// // Función para obtener un usuario por su ID
+// const getUserById = (userId) => {
+//   // Se Obtiene el usuario de la base de datos según su ID
+//   // y devuelve el objeto de usuario
+// };
+
+// const saveUser = (user) => {
+// };
+
+// // Llama a la función para modificar un usuario
+// const userId = '005'; // Reemplaza con el ID del usuario que deseas modificar
+// const newPassword = 'juanddas'; // Lo remplaza con la nueva contraseña
+
+// updateUser(userId, newPassword);
--- models/userModel.js
+++ models/userModel.js
@@ -13,7 +13,9 @@ const userSchema = new mongoose.Schema({
 
     username: {type:String, required:true},
     email: {type:String, required:true},
-    password: {type:String, required:true}
+    password: {type:String, required:true},
+    picture: {type:String},
+    role: {type:String, default:"user"}
 });
 
 module.exports = mongoose.model('Usuarios', userSchema);
